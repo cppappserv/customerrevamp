7a66c7a3fbb703f19a52ef2df8725923c2d33a3e
AQUA\supram.maharwantijo <supram.maharwantijo@cpp.co.id>
2020-08-31 11:07:50 +0700

    update posisi login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2969,8 +2969,8 @@ window.application = new Application();
 <body>
 <div id="ID0101_Login_1">
 <div class="w3-content w3-section" style="max-height:100%">
-  <img class="mySlides" src="{{asset('image/ON_DUTY.png')}}" style="height:100%">
-  <img class="mySlides" src="{{asset('image/IMG_2268.png')}}" style="height:100%">
+  <img class="mySlides" src="{{asset('image/ON_DUTY.png')}}" style="width:84.7%; height:100%;" >
+  <img class="mySlides" src="{{asset('image/IMG_2268.png')}}" style="width:84.7%; height:100%;">
   
 </div>
 
